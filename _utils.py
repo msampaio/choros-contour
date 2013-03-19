8ae5715584ead1ed7440e8c29fd6ec78b7e49696
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-03-19 07:27:41 -0300

    Insert initial code for mockup

---
+++ _utils.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def flatten(seq):
+    """Flatten Sequences.
+
+    >>> flatten([[0, 1], [2, 3]])
+    [0, 1, 2, 3]
+    """
+
+    return [item for sublist in seq for item in sublist]
--- mockup.py
+++ mockup.py
@@ -2,12 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import core
+import _utils
+from collections import defaultdict
 
 
-def make_all_phrases():
-    files_list = core.files_list()
-    return [core.make_phrases(f) for f in files_list]
+def phrase_contour_reduction(pickle_file):
+    phrases = core.load_pickle(pickle_file)
+    all_phrases = _utils.flatten(phrases)
+    return [phr.contour.reduction_morris()[0] for phr in all_phrases]
+
+def sort_phrases(contour_list):
+    # loop duplication
+
+    size = len(contour_list)
+
+    d = defaultdict(int)
+    for cseg in contour_list:
+        el = tuple(cseg)
+        d[el] += 1
+
+    sorted_data = sorted(d.items(), key=lambda x: x[1], reverse=True)
+    print "Cseg --- percentage --- ocurrences"
+    for t_cseg, n in sorted_data:
+        cseg = core.Contour(t_cseg)
+        percentage = n / float(size) * 100
+        print "{0} {1:.2f}% ({2})".format(cseg, percentage, n)
 
 
-def phrase_contour_reduction(phrases):
-    return [phr.contour.reduction_morris() for phr in phrases]
+if __name__ == '__main__':
+    pickle_file = 'pixinguinha'
+    data = phrase_contour_reduction(pickle_file)
+    sort_phrases(data)
