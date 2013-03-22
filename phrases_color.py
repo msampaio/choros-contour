c3745c6fdbfccf23fc92186a20f5d6779b87c117
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-03-22 19:25:19 -0300

    Insert file to create colored files in terminal

---
+++ phrases_color.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import core
+
+
+def color_run(filename):
+    print "Precessing song... {0}".format(os.path.basename(filename))
+    basename = filename.split('.')[0]
+    core.color_phrase_obj(basename).write('musicxml', basename + ' - color.xml')
+
+    
+if __name__ == '__main__':
+    if (len(sys.argv) > 1):
+        filenames = sys.argv[1:]
+        for f in filenames:
+            color_run(f)
+    else:
+        print "Insert xml filename as argument:\npython enumerate foo.xml"
