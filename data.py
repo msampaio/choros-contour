be304f7c8d6ae3d65f5d644aea6020df4e8279a1
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-04-02 10:57:19 -0300

    Refactoration

--- data.py
+++ data.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import core
+import os
+import pickle
+import phrase
+
+
+def save_pickle(filename, data):
+    with open(os.path.join("data", filename), 'w') as fileobj:
+        pickle.dump(data, fileobj)
+
+
+def load_pickle(filename):
+    with open(os.path.join("data", filename)) as fileobj:
+        return pickle.load(fileobj)
 
 
 if __name__ == '__main__':
-    pixinguinha = core.make_phrase_collection("O Melhor de Pixinguinha")
-    core.save_pickle("pixinguinha", pixinguinha)
+    pixinguinha = phrase.make_phrase_collection("O Melhor de Pixinguinha", False)
+    save_pickle("pixinguinha", pixinguinha)
