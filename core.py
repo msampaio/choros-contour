f933dbed1e0113fb6f0c7f10385bd5e3359cf28d
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-04-02 09:36:49 -0300

    Option to create Phrase object without music21
    * implements time signature attribute for Phrase objects

--- core.py
+++ core.py
@@ -9,12 +9,8 @@ from music21.contour import Contour
 
 
 class Phrase(object):
-    # with music21 score
-    def __init__(self, score, piece='', composer='', filename='', collection='', number=0, size=0, contour=None, contour_size=0):
+    def __init__(self, score='', piece='', composer='', filename='', collection='', number=0, size=0, contour=None, contour_size=0, time_signature=(0, 0)):
         self.score = score
-
-    # without music21 score
-    # def __init__(self, piece='', composer='', filename='', collection='', number=0, size=0, contour=None, contour_size=0):
         self.piece = piece
         self.composer = composer
         self.filename = filename
@@ -23,6 +19,7 @@ class Phrase(object):
         self.size = size
         self.contour = contour
         self.contour_size = contour_size
+        self.time_signature = time_signature
 
     def __repr__(self):
         return "<Phrase: {0}. {1}:{2}>".format(self.piece, self.collection, self.number)
@@ -38,7 +35,10 @@ def m21_data(x_name):
     composer = song.metadata.composer
     collection = os.path.basename(os.path.dirname(x_name))
     flatten_obj = song.flat.notesAndRests
-    return flatten_obj, piece, composer, collection
+    time_signature_obj = song.flat.getElementsByClass(music21.meter.TimeSignature)[0]
+    time_signature = time_signature_obj.numerator, time_signature_obj.denominator
+
+    return flatten_obj, piece, composer, collection, time_signature
 
 def phrase_locations_parser(p_name):
     """Returns a list with event numbers of phrases from a .phrase
@@ -87,30 +87,32 @@ def color_phrase_obj(basename):
     return song
 
 
-def make_phrase_obj(basename):
+def make_phrase_obj(basename, music21_obj=True):
     """Returns a list of Phrase objects with each phrase of a given
     file path. The file path must not have extension.
     """
 
     p_name = basename + '.phrase'
     x_name = basename + '.xml'
-    flatten_obj, piece, composer, collection = m21_data(x_name)
+    flatten_obj, piece, composer, collection, time_signature = m21_data(x_name)
     phrase_locations = phrase_locations_parser(p_name)
     phrases_obj = split_phrase(flatten_obj, phrase_locations)
     phrases_number = len(phrases_obj)
     print "Making {0} phrases of {1}".format(phrases_number, piece)
-    
+
     result = []
     for number, phr in enumerate(phrases_obj):
         number = number + 1
         size = len(phr)
         contour = Contour(phr)
         contour_size = len(contour)
-        # with music21 object
-        result.append(Phrase(phr, piece, composer, x_name, collection, number, size, contour, contour_size))
 
-        # without music21 score
-        # result.append(Phrase(piece, composer, x_name, collection, number, size, contour, contour_size))
+        # option to create Phrase object without music21 attribute to
+        # save in pickle
+        if not music21_obj:
+            phr = ''
+
+        result.append(Phrase(phr, piece, composer, x_name, collection, number, size, contour, contour_size, time_signature))
 
     return result
 
@@ -133,12 +135,12 @@ def filenames_list(collection, extension='phrase'):
     return [__path_without_extension(filename) for filename in phrase_files]
 
 
-def make_phrase_collection(collection):
+def make_phrase_collection(collection, music21_obj=True):
     """Returns a list of phrases objects separated by piece.
     """
 
     files = filenames_list(collection)
-    return [make_phrase_obj(f) for f in files]
+    return [make_phrase_obj(f, music21_obj) for f in files]
 
 
 def save_pickle(filename, data):
