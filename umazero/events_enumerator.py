5dc4d809246645d600e1547ee1621931b5c6b134
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-04-12 11:51:29 -0300

    Enumerate xml file instead of pdf file

--- umazero/events_enumerator.py
+++ umazero/events_enumerator.py
@@ -2,60 +2,32 @@
 
 import os
 import sys
-import shutil
-import _utils
-import music21
-
-def parse_music(filename):
-    score = music21.parse(filename)
-    try:
-        score = score.expandRepeats()
-    except music21.repeat.ExpanderException:
-        print "Warning: can't expand repetitions.", score
-
-    return score
+import re
+import song
 
 
-def count_items(score):
-    notes = score.parts[0].flat.notesAndRests
-    for x, note in enumerate(notes, 1):
-        if x % 5 == 0:
-            note.addLyric(x)
+def get_songs_filenames(collections_dir):
+    collections = os.listdir(collections_dir)
+    if '.DS_Store' in collections:
+        collections.remove('.DS_Store')
 
+    pattern = '.*\.xml$'
 
-def generate_pdf(filename):
-    # If we use a file with spaces we'll have problems
+    result = []
+    for collection in collections:
+        collpath = os.path.join(collections_dir, collection)
+        files = os.listdir(collpath)
+        songs = (os.path.join(collpath, re.search(pattern, f).string) for f in files if re.search(pattern, f))
+        result.extend(songs)
 
-    # copy files to temporary directory and rename files
-    tmpdir = '/tmp/choros'
-    _utils.mkdir(tmpdir)
-    dirname = os.path.dirname(filename)
-    basename = os.path.basename(filename)
-    shutil.copy(filename, tmpdir)
-    tmpname = os.path.join(tmpdir, basename)
-    nospacename = os.path.splitext(tmpname)[0].replace(" ", "-")
-
-    # count items and generate pdf
-    score = parse_music(filename)
-    count_items(score)
-    lily = music21.lily.translate.LilypondConverter()
-    lily.loadObjectFromScore(score)
-    lily.createPDF(nospacename)
-
-    # rename and move to original directory
-    new_name = os.path.join(tmpdir, basename.split('.')[0] + '.pdf')
-    old_name = nospacename + ".pdf"
-    os.rename(old_name, new_name)
-    shutil.copy(new_name, dirname)
-
-    # Music21 leaves the lilypond file behind. Let's delete it.
-    for f in tmpname, new_name, nospacename:
-        os.remove(f)
+    return result
 
 if __name__ == '__main__':
     if (len(sys.argv) > 1):
-        filenames = sys.argv[1:]
-        for f in filenames:
-            generate_pdf(f)
+        collections_dir = sys.argv[1]
+        for f in get_songs_filenames(collections_dir):
+            print "Processing {0}...".format(f)
+            s = song.make_song(f, True)
+            s.xml_write('numbered')
     else:
-        print "Insert xml filename as argument:\npython enumerate foo.xml"
+        print "Insert the correct corpus directory"
