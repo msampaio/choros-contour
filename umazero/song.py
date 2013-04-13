02ec56848ca0bddee251bdb521f2ff8a34dbb023
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-04-13 11:23:55 -0300

    Song class refactoration

--- umazero/song.py
+++ umazero/song.py
@@ -2,23 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import os
-import copy
 import music21
 
 
 class Song(object):
-    def __init__(self, score, measures, params, pickup, collection, filename):
-        self.score = score
-        self.title = score.metadata.title
-        self.composer = " ".join(score.metadata.composer.replace('\n', ' ').split())
-        self.collection = collection
-        self.measures = measures
-        self.params = params
-        self.pickup = pickup
-        time_signature_obj = self.params['time_signature']
-        self.time_signature = time_signature_obj.numerator, time_signature_obj.denominator
-        self.key_signature = self.params['key_signature'].sharps
-        self.filename = filename
+    def __init__(self, data):
+
+        self.filename = data['filename']
+
+        # metadata
+        self.collection = data['collection']
+        self.title = data['title']
+        self.composer = data['composer']
+
+        # music
+        self.score = data['score']
+        self.measures = data['measures']
+        self.params = data['params']
+        self.time = data['time_signature']
+        self.meter = data['meter']
+        self.pickup = data['pickup']
+        self.key = data['key']
+        self.mode = data['mode']
 
     def __repr__(self):
         return "<Song: {0}. {1}>".format(self.title, self.collection)
@@ -35,64 +40,8 @@ class Song(object):
         print "Writing xml file in {0}".format(dest)
         self.score.write('musicxml', dest)
 
-    def get_phrase(self, initial, final):
 
-        def make_measure(measure, params, keep_list, n):
-            new_measure = music21.stream.Measure()
-            if measure.number == 0:
-                new_measure.pickup = True
-            else:
-                new_measure.pickup = None
-            # insert params only in phrase first measure
-            if n == 0:
-                for values in params.values():
-                    new_measure.append(values)
-            for event in measure.notesAndRests:
-                if event.event_number in keep_list:
-                    new_measure.append(event)
-            if measure.notesAndRests[0] != new_measure.notesAndRests[0]:
-                new_measure.pickup = True
-
-            return new_measure
-
-        def make_stream(measures):
-            new_score = music21.stream.Stream()
-            for measure in measures:
-                new_score.append(measure)
-            return new_score
-
-        keep_list = range(initial, final + 1)
-        measures = self.measures
-        params = self.params
-
-        new_score = music21.stream.Stream()
-        new_score.initial = initial
-        new_score.final = final
-        new_score.pickup = False
-
-        measure_number = 1
-        first_measure = 0
-
-        for n, measure in enumerate(measures):
-            if any([(ev in keep_list) for ev in measure.events]):
-                first_measure += 1
-                if first_measure == 1:
-                    n = 0
-                new_measure = make_measure(measure, params, keep_list, n)
-                if new_measure.pickup:
-                    new_score.pickup = True
-                    new_measure.pickup = None
-                    new_measure.number = 0
-                    measure_number = 0
-                else:
-                    new_measure.number = measure_number
-                measure_number += 1
-                new_score.append(new_measure)
-
-        return new_score
-
-
-def make_song(xml_name, number_show=False):
+def make_song(filename, number_show=False):
 
     def get_parameters(measures):
         m1 = measures[0]
@@ -103,16 +52,19 @@ def make_song(xml_name, number_show=False):
         params['time_signature'] = m1.getElementsByClass('TimeSignature')[0]
         return params
 
-    score = music21.parse(xml_name)
+    score = music21.parse(filename)
     part = score.getElementsByClass('Part')[0]
     measures = part.getElementsByClass('Measure')
     params = get_parameters(measures)
+    time_signature_obj = params['time_signature']
+    key, mode = params['key_signature'].pitchAndMode
 
     if measures[0].number == 0:
         pickup = True
     else:
         pickup = False
 
+    # enumerate events
     event_n = 0
     for measure in part:
         if type(measure) == music21.stream.Measure:
@@ -121,9 +73,28 @@ def make_song(xml_name, number_show=False):
                 if type(el) in (music21.note.Note, music21.note.Rest):
                     event_n += 1
                     el.event_number = event_n
+                    # show enumeation as lyrics
                     if number_show:
                         el.lyric = event_n
                     measure_events.append(event_n)
             measure.events = measure_events
-    collection = os.path.basename(os.path.dirname(xml_name))
-    return Song(score, measures, params, pickup, collection, xml_name)
+
+    data = {}
+    data['filename'] = filename
+
+    # metadata
+    data['collection'] = os.path.basename(os.path.dirname(filename))
+    data['title'] = score.metadata.title
+    data['composer'] = " ".join(score.metadata.composer.replace('\n', ' ').split())
+
+    # music
+    data['score'] = score
+    data['measures'] = measures
+    data['params'] = params
+    data['time_signature'] = str(time_signature_obj)
+    data['meter'] = time_signature_obj.beatCountName
+    data['pickup'] = pickup
+    data['key'] = key
+    data['mode'] = mode
+
+    return Song(data)
