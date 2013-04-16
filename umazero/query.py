1e3e0f584f3391bc280126a976a83a606dffaf13
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-04-16 11:24:59 -0300

    AllMusicUnits class creation

--- umazero/query.py
+++ umazero/query.py
@@ -6,67 +6,72 @@ import _utils
 import data
 
 
-class AllPhrases(object):
-    def __init__(self, songs_phrases, stream):
-        self.songs_phrases = songs_phrases
-        self.flatten = _utils.flatten(songs_phrases)
-        self.songs_number = len(songs_phrases)
-        self.phrases_number = len(self.flatten)
-        self.stream = stream
+class AllMusicUnits(object):
+    def __init__(self, units, save=False):
+        self.save = save
+        self.units = units
+        self.units_number = len(units)
+
+
 
     def __repr__(self):
-        return "<AllPhrases: {0} songs, {1} phrases. Stream {2}>".format(self.songs_number, self.phrases_number, self.stream)
+        return "<AllMusicUnits: {0} units>".format(self.units_number)
 
     def getByIndex(self, index):
-        return self.flatten[index]
+        return self.units[index]
 
     def getByComposer(self, composer):
-        result = [[phr for phr in song_phrases if phr.composer == composer] for song_phrases in self.songs_phrases]
-        return AllPhrases([el for el in result if el], self.stream)
+        result = [u for u in self.units if u.composer == composer]
+        return AllMusicUnits([el for el in result if el], self.save)
 
     def getByTitle(self, title):
-        result = [[phr for phr in song_phrases if phr.title == title] for song_phrases in self.songs_phrases]
-        return AllPhrases([el for el in result if el], self.stream)
+        result = [u for u in self.units if u.title == title]
+        return AllMusicUnits([el for el in result if el], self.save)
 
     def getByAmbitus(self, ambitus):
-        result = [[phr for phr in song_phrases if phr.ambitus == ambitus] for song_phrases in self.songs_phrases]
-        return AllPhrases([el for el in result if el], self.stream)
+        result = [u for u in self.units if u.ambitus == ambitus]
+        return AllMusicUnits([el for el in result if el], self.save)
 
-    def getByContourPrime(self, contour_prime):
-        result = [[phr for phr in song_phrases if phr.contour.reduction_morris()[0] == contour_prime] for song_phrases in self.songs_phrases]
-        return AllPhrases([el for el in result if el], self.stream)
+    def getByContourPrime(self, contourprime):
+        result = [u for u in self.units if u.contour_prime == contourprime]
+        return AllMusicUnits([el for el in result if el], self.save)
 
     def getByPickup(self, pickup=True):
-        result = [[phr for phr in song_phrases if phr.pickup == pickup] for song_phrases in self.songs_phrases]
-        return AllPhrases([el for el in result if el], self.stream)
-
-    def make_pairs(self, offset=2, attribute=None, fn=None):
-        pairs = []
-        for song_phrases in self.songs_phrases:
-            apObj = AllPhrases([song_phrases], True)
-            size = apObj.phrases_number
-            for i in range(1, size, offset):
-                if fn:
-                    pairs.append((fn(getattr(apObj.getByIndex(i - 1), attribute)),
-                                  fn(getattr(apObj.getByIndex(i), attribute))))
-                elif attribute:
-                    pairs.append((getattr(apObj.getByIndex(i - 1), attribute),
-                                  getattr(apObj.getByIndex(i), attribute)))
-                else:
-                    pairs.append((apObj.getByIndex(i - 1), apObj.getByIndex(i)))
-        return pairs
+        result = [u for u in self.units if u.pickup == pickup]
+        return AllMusicUnits([el for el in result if el], self.save)
 
 
-def make_allphrases(stream=True):
-    phrases = []
+def getUnitsData(units):
+
+    def getData(unit, attrib):
+        s = set()
+        for unit in units:
+            s.add(getattr(unit, attrib))
+        return sorted(s)
+
+    data = {}
+    data['allComposers'] = getData(units, 'composer')
+    data['allTitles'] = getData(units, 'title')
+    data['allCollections'] = getData(units, 'collection')
+    data['allContours'] = getData(units, 'contour')
+    data['allContourSizes'] = getData(units, 'contour_size')
+    data['allContourPrimes'] = getData(units, 'contour_prime')
+    data['allAmbitus'] = getData(units, 'ambitus')
+    data['allTimeSignatures'] = getData(units, 'time_signature')
+    data['allMeters'] = getData(units, 'meter')
+
+    return data
+
+def makeAllMusicUnits(save=False):
+    units = []
     for coll in _utils.collections_list('choros-corpus'):
         print "Processing collection {0}...".format(coll)
         try:
-            if stream:
-                coll_data = phrase.make_phrase_collection(coll)
+            if save:
+                songs = song.makeSongCollection(coll, save)
             else:
-                coll_data = data.load_pickle(coll)
-            phrases.extend(coll_data)
+                songs = data.load_pickle(coll)
+            units.extend([s.subUnits for s in songs])
         except:
             print "No .phrase or .xml phrases in collection {0}".format(coll)
-    return AllPhrases(phrases, stream)
+    return AllMusicUnits(units[0], save)
