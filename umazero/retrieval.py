efd76163b5fcd66f691d6f7e683cffc302a0c8f5
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-04-23 12:03:17 -0300

    Documentation of functions

--- umazero/retrieval.py
+++ umazero/retrieval.py
@@ -9,25 +9,36 @@ import query
 
 
 def save_pickle(typeof, data):
+    """Save the given data in the file with typeof name."""
+
     _utils.mkdir("data")
     with open(os.path.join("data", typeof), 'w') as fileobj:
         pickle.dump(data, fileobj)
 
 
 def load_pickle(typeof):
+    """Load pickle file with name typeof."""
+
     with open(os.path.join("data", typeof)) as fileobj:
         return pickle.load(fileobj)
 
 
 def loadSongs():
+    """Load all Song objects saved in 'songs' pickle file"""
+
     return load_pickle('songs')
 
 
 def loadMusicUnits():
+    """Load all MusicUnits objects saved in 'units' pickle file"""
+
     return load_pickle('units')
 
 
 def run():
+    """Save pickle files for Song and MusicUnit objects of all files
+    in collections in 'choros-corpus' directory."""
+
     # songs
     songs = []
     for coll in _utils.collections_list('choros-corpus'):
