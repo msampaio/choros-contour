db0907d6ed907ed9deb252a004d9ab9265acc066
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-08-04 12:11:22 -0300

    Beat content extraction function implementation. Solve part of #99

--- umazero/duration.py
+++ umazero/duration.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import Counter
+
 
 def durations(notesSeq):
     """Return a sequence of durations of a given sequence of notes."""
@@ -23,3 +25,45 @@ def focalPointProportion(seg):
     totalLength = seg.totalLength
 
     return highestNoteOffset / totalLength
+
+
+def splitByBeat(score, durations=True, count=True):
+    def aux(sequence, ind, i):
+        return tuple(sequence[ind[i]:ind[i + 1]])
+
+    def noteOrRest(event):
+        if event.isNote:
+            string = 'note'
+        else:
+            string = 'rest'
+
+        return string, event.quarterLength
+
+    flatten = score.flat.notesAndRests
+    quarterLengthSeq = []
+    ind = []
+    n = 0
+
+    for el in flatten:
+        beat = el.beat
+        if beat % 1 == 0:
+            ind.append(n)
+        n += 1
+
+    ind.append(len(flatten))
+    split = []
+    durationsSeq = []
+
+    for i in range(len(ind) - 1):
+        notes = aux(flatten, ind, i)
+        split.append(notes)
+        if durations:
+            durationsSeq.append(tuple([noteOrRest(note) for note in notes]))
+
+    if durations:
+        split = durationsSeq
+
+    if count:
+        split = Counter(split)
+
+    return split
