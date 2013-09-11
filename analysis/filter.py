c1716882bf1f6294cf0785f28ec7b8b29421233e
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-09-11 08:21:34 -0300

    Implementation of Pieces filter class. Solve part of #110

--- analysis/filter.py
+++ analysis/filter.py
@@ -61,6 +61,77 @@ class Composers(object):
         return makeComposers([obj for obj in self.objects if deathYear in obj.deathYear])
 
 
+class Pieces(object):
+    """Class for Piece objects filtering."""
+
+    def __init__(self):
+        self.objects = None
+        self.titles = None
+        self.composers = None
+        self.cities = None
+        self.years = None
+        self.size = None
+
+    def __eq__(self, other):
+        return _utils.equalityComparisons(self, other)
+
+    def __ne__(self, other):
+        return _utils.equalityComparisons(self, other, True)
+
+    def __repr__(self):
+        return "<Pieces: {0}>".format(self.size)
+
+    def getByTitle(self, title):
+        return makePieces([obj for obj in self.objects if title in obj.title])
+
+    def getByCity(self, city):
+        return makePieces([obj for obj in self.objects if city in obj.city])
+
+    def getByYear(self, year):
+        if type(year) == int:
+            year = str(year)
+        return makePieces([obj for obj in self.objects if year in obj.year])
+
+    def getByComposerName(self, composerName):
+        def aux(objects, composerName):
+            return composerName in ', '.join([o.name for o in objects.composer])
+        return makePieces([obj for obj in self.objects if aux(obj, composerName)])
+
+    def getByComposerGender(self, composerGender):
+        def aux(objects, composerGender):
+            return composerGender in ', '.join([o.gender for o in objects.composer])
+        return makePieces([obj for obj in self.objects if aux(obj, composerGender)])
+
+    def getByComposerInstrument(self, composerInstrument):
+        def aux(objects, composerInstrument):
+            return composerInstrument in ', '.join([o.mainInstrument for o in objects.composer])
+        return makePieces([obj for obj in self.objects if aux(obj, composerInstrument)])
+
+    def getByComposerBornYear(self, composerBornYear):
+        def aux(objects, composerBornYear):
+            if type(composerBornYear) == int:
+                composerBornYear = str(composerBornYear)
+            return composerBornYear in ', '.join([o.bornYear for o in objects.composer])
+        return makePieces([obj for obj in self.objects if aux(obj, composerBornYear)])
+
+    def getByComposerBornCity(self, composerBornCity):
+        def aux(objects, composerBornCity):
+            return composerBornCity in ', '.join([o.bornCity.name for o in objects.composer if o.bornCity])
+        return makePieces([obj for obj in self.objects if aux(obj, composerBornCity)])
+
+    def getByComposerDeathYear(self, composerDeathYear):
+        def aux(objects, composerDeathYear):
+            if type(composerDeathYear) == int:
+                composerDeathYear = str(composerDeathYear)
+            return composerDeathYear in ', '.join([o.deathYear for o in objects.composer])
+        return makePieces([obj for obj in self.objects if aux(obj, composerDeathYear)])
+
+    def getByComposerDeathCity(self, composerDeathCity):
+        def aux(objects, composerDeathCity):
+            return composerDeathCity in ', '.join([o.deathCity.name for o in objects.composer if o.deathCity])
+        return makePieces([obj for obj in self.objects if aux(obj, composerDeathCity)])
+
+
 def makeComposers(composersObjList):
     """Make a Composers object from a list of Composer objects."""
 
@@ -74,6 +145,21 @@ def makeComposers(composersObjList):
     composers.size = len(composers.objects)
 
     return composers
+
+
+def makePieces(piecesObjList):
+    """Make a Pieces object from a list of Piece objects."""
+
+    pieces = Pieces()
+    pieces.objects = _utils.organizeAndSort(piecesObjList)
+    pieces.composers = makeComposers(_utils.flatten([obj.composer for obj in pieces.objects if obj.composer]))
+    pieces.titles = _utils.organizeAndSort([obj.title for obj in piecesObjList if obj.title])
+    pieces.cities = _utils.organizeAndSort([obj.city for obj in piecesObjList if obj.city])
+    pieces.years = _utils.organizeAndSort([obj.year for obj in piecesObjList if obj.year])
+    pieces.size = len(pieces.objects)
+
+    return pieces
+
 
 def getByComposer(objList, composerName):
     """Return a filtered list of objects by a given composer name."""
