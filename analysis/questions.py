bbdddd3a304b5e1dbb069988632928f0f95b42df
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-09-26 12:01:11 -0300

    Composers comparison functions

---
+++ analysis/questions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def compareComposers(segmentsObj, method, *composers):
+    """Return the data of a given list of composers.
+
+    >>> compareComposers(segmentsObj, 'countIntervals', 'Pixinguinha', 'Pecci', 'Nazareth')
+    """
+
+    def aux(segmentsObj, method, composer):
+        dic[composer] = segmentsObj.getByComposerName(composer).__getattribute__(method)()
+
+    dic = {}
+    for composer in composers:
+        aux(segmentsObj, method, composer)
+    return dic
+
+
+def compareComposerWithAll(segmentsObj, method, composerName):
+    """Return the data of a given list of composers.
+
+    >>> compareComposersWithAll(segmentsObj, 'countIntervals', 'Pixinguinha')
+    """
+
+    dic = {}
+    dic[composerName] = segmentsObj.getByComposerName(composerName).__getattribute__(method)()
+    dic['All composers'] = segmentsObj.__getattribute__(method)()
+
+    return dic
+
+
+def compareComposerWithOthers(segmentsObj, method, composerName):
+    """Return the data of a given list of composers.
+
+    >>> compareComposersWithOthers(segmentsObj, 'countIntervals', 'Pixinguinha')
+    """
+
+    dic = {}
+    dic[composerName] = segmentsObj.getByComposerName(composerName).__getattribute__(method)()
+    dic['Others'] = segmentsObj.getByComposerName(composerName, True).__getattribute__(method)()
+
+    return dic
