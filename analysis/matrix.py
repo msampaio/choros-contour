a1d0c2ce46cf3b1997f5e076dfd7d43ef8f866dd
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-09-28 12:17:21 -0300

    Unused function removal. Solve part of #110

--- analysis/matrix.py
+++ analysis/matrix.py
@@ -114,24 +114,3 @@ def makeOutMatrix(segmentsObj, nestedObj, getFn, attrib, countFn):
     outMatrix.columnsValuesSet = sorted(list(columnsValuesSet))
 
     return outMatrix
-
-
-def countedToCsv(dic, filename='/tmp/foo.csv'):
-    """Save a given dictionary in a given csv file."""
-
-    header = dic.keys()
-    rowOne = set()
-    for countObj in dic.values():
-        for key in countObj.keys():
-            rowOne.add(key)
-    rowOne = sorted(list(rowOne))
-
-    csvTable = [copy.deepcopy(header)]
-    csvTable[0].insert(0, '')
-    for r in rowOne:
-        csvRow = [r]
-        for h in header:
-            csvRow.append(dic[h][r])
-        csvTable.append(csvRow)
-
-    _utils.saveCsvFile(csvTable, filename)
