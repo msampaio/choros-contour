7370b41d2ea9c86f981a2b5587d2abd7c7c82046
Marcos da Silva Sampaio <marcos@sampaio.me>
2013-10-02 18:29:06 -0300

    Fix module's init file. Solve part of #110

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -2,21 +2,15 @@
 from idcode import idCodeParser
 from idcode import idCodeMaker
 
-from core import makeCity
-from core import makeComposer
-from core import makePiece
-from core import makeCollection
-from core import makeSource
+from structure import makeComposer
+from structure import makePiece
+from structure import makeCollection
+from structure import makeSource
+from structure import makeSegments
+
+from query import makeQuery
 
 from retrieval import getMusicologicalInfo
 from retrieval import saveAll
+from retrieval import saveSegments
 from retrieval import loadAll
-
-from segment import makeSegment
-
-from sift import makeComposers
-from sift import makePieces
-from sift import makeSources
-from sift import makeSegments
-
-from questions import makeCollectedDataTables
